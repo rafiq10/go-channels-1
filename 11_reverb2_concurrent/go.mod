cbc9ed9c9fef4128c464a5ad5a6cf5dc21a43d7f
rafiq10 <rafiq10@gmail.com>
2022-08-29 22:09:06 +0200

    Excercise 8.4

---
+++ 11_reverb2_concurrent/go.mod
@@ -0,0 +1,3 @@
+module go-channels-1/11_reverb2_concurrent
+
+go 1.18
---
+++ 11_reverb2_concurrent/reverb2.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"sync"
+	"time"
+)
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+//!+
+func handleConn(c net.Conn) {
+	wg := sync.WaitGroup{}
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		wg.Add(1)
+		go echo(c, input.Text(), 1*time.Second)
+	}
+	wg.Wait()
+	// NOTE: ignoring potential errors from input.Err()
+	c.Close()
+}
+
+//!-
+
+func main() {
+	l, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Print(err) // e.g., connection aborted
+			continue
+		}
+		go handleConn(conn)
+	}
+}
